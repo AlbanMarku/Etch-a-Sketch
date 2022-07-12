4ecfb31b1fd3511fb8162926b84ea6a10db36c22
AlbanMarku <albanmarku@outlook.com>
2022-07-12 20:27:25 +0100

    Dynamic grid size added.

--- jscript.js
+++ jscript.js
@@ -1,10 +1,32 @@
 const boxContainer = document.getElementById("grid-container");
+const slider = document.getElementById("slider");
+const numberOnSlider = document.getElementById("value");
+
+window.onload = () => {
+    numberOnSlider.innerHTML = slider.value;
+    let finalInput = numberOnSlider.innerHTML;
+    updateGridSize(finalInput);
+  }
+
+slider.oninput = function() {
+    numberOnSlider.innerHTML = this.value;
+    let finalInput = numberOnSlider.innerHTML;
+    clearGrid();
+    updateGridSize(finalInput);
+  }
 
 
-let amount = 25;
+function updateGridSize(amount) {
+    boxContainer.style.gridTemplateColumns = `repeat(${amount},auto)`;
+    boxContainer.style.gridTemplateColumns = `repeat(${amount},auto)`;
 
-for (let i = 0; i < amount; i++) {
-    const gridElement = document.createElement("div");
-    gridElement.classList.add("grid-item");
-    boxContainer.appendChild(gridElement);    
+    for (let i = 0; i < amount * amount; i++) {
+        const gridElement = document.createElement("div");
+        gridElement.classList.add("grid-item");
+        boxContainer.appendChild(gridElement);    
+    }
+}
+
+function clearGrid() {
+    boxContainer.innerHTML = ""
 }
