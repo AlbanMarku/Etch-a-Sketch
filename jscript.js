af75a471d0868aefd34a7119d42c2d1d59d82397
AlbanMarku <albanmarku@outlook.com>
2022-07-13 21:00:04 +0100

    Now can draw on grid with color picker while held down.

--- jscript.js
+++ jscript.js
@@ -4,6 +4,10 @@ const numberOnSlider = document.getElementById("value");
 const picker = document.getElementById("colorPicker");
 const butt = document.querySelectorAll("button");
 
+let mouseDown = false
+document.body.onmousedown = () => (mouseDown = true)
+document.body.onmouseup = () => (mouseDown = false)
+
 window.onload = () => {
     numberOnSlider.innerHTML = slider.value;
     let finalInput = numberOnSlider.innerHTML;
@@ -24,6 +28,7 @@ function updateGridSize(amount) {
 
     for (let i = 0; i < amount * amount; i++) {
         const gridElement = document.createElement("div");
+        gridElement.removeAttribute("draggable");
         gridElement.addEventListener('mouseover', changeColor)
         gridElement.addEventListener('mousedown', changeColor)
         boxContainer.appendChild(gridElement);    
@@ -38,7 +43,7 @@ function changeColor(e) {
   if (e.type === 'mouseover' && !mouseDown) {
     return
   } else {
-    console.log("yes");
+    e.target.style.backgroundColor = picker.value;
   }
 }
 
